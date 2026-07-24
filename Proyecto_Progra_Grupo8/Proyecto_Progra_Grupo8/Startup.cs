fb686b151f73adf6b629ec8221c4560cf466ceee
robertoAcunaBallestero <robertoacu953@gmail.com>
2026-07-23 18:31:58 -0600

    Proyecto nuevo

---
+++ Proyecto_Progra_Grupo8/Proyecto_Progra_Grupo8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Progra_Grupo8.Startup))]
+namespace Proyecto_Progra_Grupo8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
